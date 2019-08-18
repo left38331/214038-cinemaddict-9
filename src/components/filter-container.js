f5b912ee85c2d5f9cc462cd47b0f0682734c6479
left38331 <left38331@mail.ru>
2019-08-18 15:30:38 +0300

    add random data for cards, filters and other little things

---
+++ src/components/filter-container.js
@@ -0,0 +1,5 @@
+export const createNavagationLayout = () => `
+  <nav class="main-navigation">
+    <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+  </nav>
+`;
---
+++ src/components/footer-statistic.js
@@ -0,0 +1,5 @@
+export const createFooterStatisticLayout = (count) => `
+  <section class="footer__statistics">
+    <p>${count} movies inside</p>
+  </section>
+`;
